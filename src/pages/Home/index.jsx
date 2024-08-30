ba1da2296a131f2529d8e0e4846967225846016d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-29 21:04:47 -0300

    Card Dinamico

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,46 @@
 import Button from "../../Components/Button";
 import Card from "../../Components/Cards";
-import "./styles.css";
-import heroImage from "../../assets/heroImage.png";
-import youtubeIcon from "../../assets/youtubeIcon.png";
-import tiktokIcon from "../../assets/tiktokIcon.png";
-import facebookIcon from "../../assets/facebookIcon.png";
-import instagramIcon from "../../assets/instagramIcon.png";
 import Text from "../../Components/Text";
+import facebookIcon from "../../assets/facebookIcon.png";
+import heroImage from "../../assets/heroImage.png";
+import instagramIcon from "../../assets/instagramIcon.png";
+import tiktokIcon from "../../assets/tiktokIcon.png";
+import youtubeIcon from "../../assets/youtubeIcon.png";
+import "./styles.css";
 function Home() {
+  const cardData = [
+    {
+      color: "#F1C2B0",
+      title: "Youtube",
+      description: "Produza conteúdo cativante e conquiste seu público com vídeos de alta qualidade no maior palco digital do mundo.",
+      number: "01",
+      icon: youtubeIcon
+    },
+    {
+      color: "#F9EB98",
+      title: "Tiktok",
+      description: "Faça sua criatividade brilhar em vídeos curtos e envolventes que vão cativar a comunidade global do TikTok.",
+      number: "02",
+      icon: tiktokIcon
+    },
+    {
+      color: "#C0D9DD",
+      title: "Facebook",
+      description: "Conecte-se com sua audiência de forma autêntica e impactante através de vídeos que se destacam no feed do Facebook.",
+      number: "03",
+      icon: facebookIcon
+    },
+    {
+      color: "#6975E8",
+      title: "Instagram",
+      description: "Compartilhe suas histórias de maneira única e conquiste milhões de likes no Instagram utilizando Stories e Reels.",
+      number: "04",
+      icon: instagramIcon
+    }
+  ];
+
+
+
   return (
     <>
       <div className="hero-image-section">
@@ -24,34 +57,18 @@ function Home() {
         <img width={"50%"} height={460} src={heroImage} />
       </div>
       <div className="card-contianer">
-        <Card
-          color="#F1C2B0"
-          title={"Youtube"}
-          description="Produza conteúdo cativante e conquiste seu público com vídeos de alta qualidade no maior palco digital do mundo."
-          number="01"
-          icon={youtubeIcon}
-        />
-        <Card
-          color="#F9EB98"
-          title="Tiktok"
-          description="Faça sua criatividade brilhar em vídeos curtos e envolventes que vão cativar a comunidade global do TikTok."
-          number="02"
-          icon={tiktokIcon}
-        />
-        <Card
-          color="#C0D9DD"
-          title="Facebook"
-          description="Conecte-se com sua audiência de forma autêntica e impactante através de vídeos que se destacam no feed do Facebook."
-          number="03"
-          icon={facebookIcon}
-        />
-        <Card
-          color="#6975E8"
-          title="Instagram"
-          description="Compartilhe suas histórias de maneira única e conquiste milhões de likes no Instagram utilizando Stories e Reels. "
-          number="04"
-          icon={instagramIcon}
-        />
+        {cardData.map((card, index) => (
+          <Card
+            key={index}
+            color={card.color}
+            title={card.title}
+            description={card.description}
+            number={card.number}
+            icon={card.icon}
+          />
+        ))}
+
+
       </div>
     </>
   );
